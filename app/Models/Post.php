68ff563ded7736cefcd78be81412bf887dd0e507
wawawa062 <crmc369@gmail.com>
2023-09-01 16:29:48 +0900

    blog投稿の一覧表示

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,5 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    public function getPaginateByLimit(int $limit_count = 1)
+    {
+    return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    }
+    
     use HasFactory;
 }
+
